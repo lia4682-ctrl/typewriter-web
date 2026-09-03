6c3d940e35c442e06e59e7023d83274a6c204f4f
lia4682-ctrl <lia4682@gmail.com>
2026-09-03 09:29:08 +0900

    Update and rename layout.jsx to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ export const metadata = {
   description: 'A vintage typewriter journaling web app',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="ko">
       <body>{children}</body>
