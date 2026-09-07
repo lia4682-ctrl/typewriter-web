8a2cbfa2736028a4fd3d8a936ba054fd0cce39ed
lia4682-ctrl <lia4682@gmail.com>
2026-09-07 10:48:09 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://szfgtyqxdmosjkjqsbvq.supabase.co'; // 뒤에 아무것도 붙이지 않음
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6Zmd0eXF4ZG1vc2pranFzYnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MDE3MjIsImV4cCI6MjEwNDA3NzcyMn0.OffYEWXnF2mkRid04MVQtVTZ7J__fGdbHHu_ZQTNmDo';
+const supabaseAnonKey = 'sb_publishable_tz3zt36eEaoPyAJ0dLUefA_NLvjiVyu';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
